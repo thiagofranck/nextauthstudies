7a2a5f623fd6baa29dcac90cae7f011e4dfa4530
Thiago Franck <thiagohenrique776@gmail.com>
2021-05-27 12:04:25 -0300

    Configurações iniciais do Provider

---
+++ contexts/AuthContext.tsx
@@ -0,0 +1,27 @@
+import { createContext } from "react";
+
+type SignInCredentials = {
+  email: string;
+  password: string;
+}
+
+type AuthContextData = {
+  signIn(credentials: SignInCredentials): Promise<void>
+  isAuthenticated: boolean;
+};
+
+const AuthContext = createContext({} as AuthContextData)
+
+export function AuthProvider({children}) {
+  const isAuthenticated = false;
+
+  function signIn({ email, password }: SignInCredentials) {
+    console.log({email, password})
+  }
+
+  return (
+    <AuthContext.Provider value={{}}>
+      {children}
+    </AuthContext.Provider>
+  )
+}
