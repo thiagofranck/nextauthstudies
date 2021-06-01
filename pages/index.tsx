678732f678f2f9cb8ce08457139544a9fbe75e6c
Thiago Franck <thiagohenrique776@gmail.com>
2021-06-01 11:41:21 -0300

    Validação de visitante

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,15 @@
-import { GetServerSideProps } from 'next';
-import { FormEvent, useContext, useState } from 'react'
-import { parseCookies } from 'nookies'
-
-import { AuthContext } from '../contexts/AuthContext';
-import styles from '../styles/Home.module.css'
+import { FormEvent, useContext, useState } from "react";
+import { parseCookies } from "nookies";
+import { AuthContext } from "../contexts/AuthContext";
+import styles from "../styles/Home.module.css";
+import { GetServerSideProps } from "next";
+import { withSSRGuest } from "../utils/withSSRGuest";
 
 export default function Home() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-  const { signIn } = useContext(AuthContext)
+  const { signIn } = useContext(AuthContext);
 
   async function handleSubmit(event: FormEvent) {
     event.preventDefault();
@@ -17,34 +17,30 @@ export default function Home() {
     const data = {
       email,
       password,
-    }
+    };
 
     await signIn(data);
   }
 
-
   return (
     <form onSubmit={handleSubmit} className={styles.container}>
-      <input type="email" value={email} onChange={e => setEmail(e.target.value)} />
-      <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
+      <input
+        type="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
       <button type="submit">Entrar</button>
     </form>
-  )
+  );
 }
 
-export const getServerSideProps: GetServerSideProps = async (ctx) => {
-  const cookies = parseCookies(ctx)
-
-  if (cookies['nextauth.token']) {
-    return {
-      redirect: {
-        destination: '/dashboard',
-        permanent: false,
-      }
-    }
-  }
-
-  return { 
+export const getServerSideProps = withSSRGuest(async (ctx) => {
+  return {
     props: {}
   }
-}
+});
