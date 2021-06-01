b13bbf172fa308228ff8eb8e7a79b9267bb3cd4f
Thiago Franck <thiagohenrique776@gmail.com>
2021-06-01 10:59:37 -0300

    Cookie no server-side

--- contexts/AuthContext.tsx
+++ contexts/AuthContext.tsx
@@ -1,6 +1,6 @@
 import { createContext, ReactNode, useEffect, useState } from "react";
-import { setCookie, parseCookies } from 'nookies'
-import Router from 'next/router';
+import { setCookie, parseCookies, destroyCookie } from "nookies";
+import Router from "next/router";
 
 import { api } from "../services/api";
 
@@ -10,14 +10,13 @@ type User = {
   roles: string[];
 };
 
-
 type SignInCredentials = {
   email: string;
   password: string;
 };
 
 type AuthContextData = {
-  signIn(credentials: SignInCredentials): Promise<void>
+  signIn(credentials: SignInCredentials): Promise<void>;
   user: User;
   isAuthenticated: boolean;
 };
@@ -26,54 +25,65 @@ type AuthProviderProps = {
   children: ReactNode;
 };
 
-export const AuthContext = createContext({} as AuthContextData)
+export const AuthContext = createContext({} as AuthContextData);
 
-export function AuthProvider({children}: AuthProviderProps) {
-  const [user, setUser] = useState<User>()
+export function signOut() {
+  destroyCookie(undefined, 'nextauth.token')
+  destroyCookie(undefined, 'nextauth.refreshToken')
+
+  Router.push('/')
+}
+
+export function AuthProvider({ children }: AuthProviderProps) {
+  const [user, setUser] = useState<User>();
   const isAuthenticated = !!user;
 
   useEffect(() => {
-    const { 'nextauth.token' : token } = parseCookies()
+    const { "nextauth.token": token } = parseCookies();
 
-    if (token) [
-      api.get('/me').then(response => {
-        const { email, permissions, roles } = response.data
+    if (token) {
+      api
+        .get("/me")
+        .then((response) => {
+          const { email, permissions, roles } = response.data;
 
-        setUser({ email, permissions, roles})
-      })
-    ]
-  }, [])
+          setUser({ email, permissions, roles });
+        })
+        .catch(() => {
+          signOut();
+        })
+    }
+  }, []);
 
   async function signIn({ email, password }: SignInCredentials) {
     try {
-      const response = await api.post('sessions', {
+      const response = await api.post("sessions", {
         email,
-        password
-      })
+        password,
+      });
 
       const { token, refreshToken, permissions, roles } = response.data;
 
-      setCookie(undefined, 'nextauth.token', token, {
+      setCookie(undefined, "nextauth.token", token, {
         maxAge: 60 * 60 * 24 * 30, // 30 days
-        path: '/'
-      })
-      setCookie(undefined, 'nextauth.refreshToken', refreshToken, {
+        path: "/",
+      });
+      setCookie(undefined, "nextauth.refreshToken", refreshToken, {
         maxAge: 60 * 60 * 24 * 30, // 30 days
-        path: '/'
-      })
-  
+        path: "/",
+      });
+
       setUser({
         email,
         permissions,
-        roles
+        roles,
+      });
 
-      })
+      api.defaults.headers["Authorization"] = `Bearer ${token}`;
 
-      api.defaults.headers['Authorization'] = `Bearer ${token}`;
-
-      Router.push('/dashboard')
+      Router.push("/dashboard");
     } catch (err) {
-      console.log(err)
+      console.log(err);
     }
   }
 
@@ -81,5 +91,5 @@ export function AuthProvider({children}: AuthProviderProps) {
     <AuthContext.Provider value={{ signIn, isAuthenticated, user }}>
       {children}
     </AuthContext.Provider>
-  )
+  );
 }
--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,15 +1,16 @@
-import { useContext, useEffect } from "react"
-import { AuthContext } from "../contexts/AuthContext"
-import { api } from "../services/api"
+import { useContext, useEffect } from "react";
+import { AuthContext } from "../contexts/AuthContext";
+import { api } from "../services/api";
 
 export default function Dashboard() {
-  const { user } = useContext(AuthContext)
+  const { user } = useContext(AuthContext);
 
   useEffect(() => {
-    api.get('/me').then(response => console.log(response))
-  }, [])
+    api
+      .get("/me")
+      .then((response) => console.log(response))
+      .catch((err) => console.log(err));
+  }, []);
 
-  return (
-    <h1>Dashboard: {user?.email}</h1>
-  )
+  return <h1>Dashboard: {user?.email}</h1>;
 }
